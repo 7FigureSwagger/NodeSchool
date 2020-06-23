b5ca22d244e9589e49749f5e9b86ee14966f0822
7figureswagger <swistek.labs@gmail.com>
2020-06-23 16:02:47 -0700

    changed the 'contents' variable to use process.argv[2] directly instead of typecasting the whole array, since input filename search is only one string

--- my-first-io.js
+++ my-first-io.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 
 fileToRead = process.argv.slice(2);
+//Ends up being an array, would require typecast to string, better to select the array element directly if input is one string
 
-let contents = fs.readFileSync(fileToRead, 'utf8');
+let contents = fs.readFileSync(process.argv[2], 'utf8');
+//Add 'utf8' as the second argument to fs to avoid having to use .toString()
+// fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
+const lines = contents.split('\n').length - 1;
 
-const temp = contents.toString().split('\n');
-
-console.log(temp.length - 1);
+console.log(lines);
 //The .length property will have one more element than the actual number of '\n' newlines.
 //counting the elements AFTER the .split() function is called
