84809af51ff665037a9ada517d4055a1f9a3c409
7figureswagger <swistek.labs@gmail.com>
2020-06-23 16:25:55 -0700

    callback checks for error, if no error present logs data received from the readFile counting number of '\n' newlines

--- my-first-async-io.js
+++ my-first-async-io.js
@@ -2,9 +2,12 @@ const fs = require('fs');
 
 let input = process.argv[2];
 
+//Take input, set encoding to avoid using toString() as before, pass in callback to collect the data asynchronously, handle errors
 fs.readFile(input, 'utf8', (err, data) => {
   if(err){
     console.log('error', err);
+  } else {
+    console.log(data.split('\n').length - 1);
   }
-})
-console.log(input);
+});
+
