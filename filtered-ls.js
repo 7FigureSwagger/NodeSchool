70982d6c14e58baa51fc363ad1e43bfc06aa93db
7figureswagger <swistek.labs@gmail.com>
2020-06-23 17:46:53 -0700

    iterating through returned list to show only files with the desired extension

--- filtered-ls.js
+++ filtered-ls.js
@@ -3,13 +3,18 @@ const path = require("path");
 
 const directoryInput = process.argv[2];
 const fileExtension = process.argv[3];
+console.log(fileExtension);
 
-console.log(
-	fs.readdir(directoryInput, (err, list) => {
-		if (err) {
-			console.log("error", err);
-		} else {
-      console.log(list);
-		}
-	})
-);
+fs.readdir(directoryInput, (err, list) => {
+	if (err) {
+		console.log("error", err);
+	} else {
+    console.log("inside")
+		list.forEach((i) => {
+			if (path.extname(i) === fileExtension) {
+				console.log(fileExtension);
+				console.log(i);
+			}
+		});
+	}
+});
