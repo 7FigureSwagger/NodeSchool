9a9c209475ed58c7b31a8a51be6da4262fadcfed
7figureswagger <swistek.labs@gmail.com>
2020-06-23 18:00:13 -0700

    do a comparion on each item in the list, add '.' to the file extension as it will not be present in the input

--- filtered-ls.js
+++ filtered-ls.js
@@ -3,16 +3,13 @@ const path = require("path");
 
 const directoryInput = process.argv[2];
 const fileExtension = process.argv[3];
-console.log(fileExtension);
 
 fs.readdir(directoryInput, (err, list) => {
 	if (err) {
 		console.log("error", err);
 	} else {
-    console.log("inside")
 		list.forEach((i) => {
-			if (path.extname(i) === fileExtension) {
-				console.log(fileExtension);
+			if (path.extname(i) === "." + fileExtension) {
 				console.log(i);
 			}
 		});
