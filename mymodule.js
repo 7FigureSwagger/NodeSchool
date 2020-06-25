b5da50a85d126ac13a0802e32a3d5a170cd40226
7figureswagger <swistek.labs@gmail.com>
2020-06-24 18:21:27 -0700

    created function, taking parameters, attempt to read file, exit early if error is present

--- mymodule.js
+++ mymodule.js
@@ -4,16 +4,51 @@ const path = require("path");
 const directoryInput = process.argv[2];
 const fileType = process.argv[3];
 
-function readData(directoryInput, fileType, callBack){
-  
+
+
+function readFile(dir, ext, callback){
+  let filteredList = [];
+  (dir, ext) => {
+    fs.readdir(dir, (err, data) => {
+      if(err){return callback(err)};
+      data.forEach((file) => {
+        // if(path.extname(file))
+        filteredList.push(file);
+      });
+    });
+  }; 
+
+  callback(null, filteredList);
 }
 
 
 
-
-fs.readdir(directoryInput, () => {
-  function readData(err, data) => {
-    if(err){ return readData(null, data)}
+// function readData(directory, extension, (e) => {console.log(e)}) {
+//   let filteredList = [];
+  
+//   fs.readdir(directory, (err, list) => {
+//     if (err) {
+//       return (err) => {};
+// 		}
     
-  }
-});
+// 		list.forEach((file) => {
+// 			if (path.extname(file) === "." + extension) {
+// 				filteredList.push(file);
+// 				console.log(filteredList);
+// 			}
+//     });
+    
+//     callback(filteredList);
+
+//   });
+//   // return filteredList;
+  
+// }
+
+
+// fs.readdir(directoryInput, () => {
+//   function readData(err, data) => {
+//     if(err){ return readData(null, data)}
+
+//   }
+// });
