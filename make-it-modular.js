f8c5b41fafb21446c289dea4e89cdf479b294d2b
7figureswagger <swistek.labs@gmail.com>
2020-06-25 17:05:02 -0700

    modular function is built, need to find bug with returned data not displaying

--- make-it-modular.js
+++ make-it-modular.js
@@ -1,10 +1,14 @@
 const mymodule = require('./mymodule');
+const { dir } = require('console');
 
 const directoryInput = process.argv[2];
 const fileType = process.argv[3];
-console.log(mymodule.readFile())
-// mymodule(directoryInput, fileType, (err, data) => {
-//   if(err){console.log(err)};
+// console.log(mymodule.readFile())
 
-//   console.log(data);
-// })
+console.log(directoryInput);
+
+mymodule(directoryInput, fileType, (err, data) => {
+  if(err){console.log(err)};
+  console.log('function call');
+  console.log(data);
+})
--- mymodule.js
+++ mymodule.js
@@ -1,50 +1,47 @@
 const fs = require("fs");
 const path = require("path");
 
+module.exports = function readFile(dir, ext, callback) {
+	function searchStrict(loc, type) {
+		let filteredList = [];
 
-module.exports = function readFile(dir, ext, callback){
+		fs.readdir(loc, (err, data) => {
+			if (err) {
+				return callback(err, data);
+			}
 
-  function searchStrict(loc, type){
-    let filteredList = [];
-  
-    fs.readdir(loc, (err, data) => {
-      if(err){return callback(err, data)};
-    
-      data.forEach((file) => {
-        if(path.extname(file) === type){
-          filteredList.push(file);
-        }
-      });
-    });
-  
-    callback(null, filteredList);
-  }; 
-
-}
+			data.forEach((file) => {
+				if (path.extname(file) === type) {
+					filteredList.push(file);
+				}
+			});
+			callback(null, filteredList);
+		});
+	}
+};
 
 
 // function readData(directory, extension, (e) => {console.log(e)}) {
 //   let filteredList = [];
-  
+
 //   fs.readdir(directory, (err, list) => {
 //     if (err) {
 //       return (err) => {};
 // 		}
-    
+
 // 		list.forEach((file) => {
 // 			if (path.extname(file) === "." + extension) {
 // 				filteredList.push(file);
 // 				console.log(filteredList);
 // 			}
 //     });
-    
+
 //     callback(filteredList);
 
 //   });
 //   // return filteredList;
-  
-// }
 
+// }
 
 // fs.readdir(directoryInput, () => {
 //   function readData(err, data) => {
