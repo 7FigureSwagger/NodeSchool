ba1d9beea4bd3536f620706e830327340ee6e3e4
7figureswagger <swistek.labs@gmail.com>
2020-06-27 18:01:06 -0700

    created get request, set encoding for response data, writing callback to handle raw data coming in

--- http-client.js
+++ http-client.js
@@ -1,3 +1,16 @@
 const http = require('http');
 
-console.log(process.argv);
+const urlSearch = process.argv[2];
+console.log(urlSearch);
+
+http.get(urlSearch, (res) => {
+ 
+  res.setEncoding('utf-8');
+  let rawData = '';
+
+  res.on('data', (chunk) => {
+    chunk += rawData;
+  })
+
+  console.log(rawData);
+})
