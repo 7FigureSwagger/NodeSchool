ae765f5244390c59290daccf9cf7bd6bb0384200
7figureswagger <swistek.labs@gmail.com>
2020-06-27 22:12:08 -0700

    counting characters recieved, concat-ing strings recieved together and outputting the string

--- http-collect.js
+++ http-collect.js
@@ -1,6 +1,21 @@
-const http = require('http');
+const http = require("http");
 
+http
+	.get(process.argv[2], (res) => {
+		let characterCount = 0;
+		let recievedData = "";
 
-http.get(process.argv[2], () => {
-  console.log('get');
-});
+		res.setEncoding("utf8");
+		res.on("data", (chunk) => {
+      characterCount += chunk.length;
+      recievedData += chunk;
+		});
+
+		res.on("error", console.error);
+
+		res.on("end", () => {
+      console.log(characterCount);
+      console.log(recievedData);
+    });
+	})
+	.on("error", console.error);
