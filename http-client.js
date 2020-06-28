147194623d41d3b7802b17b9305bf34d531c2ee5
7figureswagger <swistek.labs@gmail.com>
2020-06-27 21:30:55 -0700

    commented out some code not needed for the challenge

--- http-client.js
+++ http-client.js
@@ -1,19 +1,19 @@
 const http = require('http');
 
 const urlSearch = process.argv[2];
-console.log(urlSearch);
 
 http.get(urlSearch, (res) => {
   
   res.setEncoding('utf8');
   let recievedData = '';
 
+  //Catch data as its passed in the response
   res.on('data', (chunk) => {
     recievedData += chunk;
     console.log(chunk);
   })
-  res.on('end', () => {
-    console.log(recievedData);
+  // res.on('end', () => {
+    // console.log(recievedData);
     //Only need to do the following if response is not JSON
     // try {
     //   const parsedData = JSON.parse(rawData);
@@ -22,6 +22,6 @@ http.get(urlSearch, (res) => {
     // } catch (error) {
     //   console.log('parsing error ', error);
     // }
-  })
+  // })
 
 }).on('error', (e) => {console.error('got error', e)});
