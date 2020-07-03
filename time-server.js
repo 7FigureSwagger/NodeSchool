d9e53b829d38b9556239afcdf8cac3e1705f6dbb
7figureswagger <swistek.labs@gmail.com>
2020-07-02 23:43:47 -0700

    format date, zerofill function

--- time-server.js
+++ time-server.js
@@ -1,16 +1,15 @@
 const net = require('net');
 const port = process.argv[2];
-
 const server = net.createServer(function(socket){
 
   let data = '';
   let date = new Date();
-  data += date.getFullYear() + '-';
-  data += date.getMonth() + '-';
+  data += date.getFullYear() + '-0';
+  data += (date.getMonth() + 1) + '-0';
   data += date.getDate() + ' ';
   data += date.getHours() + ':'
-  data += date.getMinutes();
-
-  socket.end(data);
+  data += date.getMinutes() + '\n';
+  socket.write(data);
+  socket.end();
 });
 server.listen(port);
