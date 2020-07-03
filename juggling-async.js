b464e9a607ea275c6c49881245ec5991d7e8edb1
7figureswagger <swistek.labs@gmail.com>
2020-07-02 18:43:39 -0700

    code cleanup, made notes of alternate method of handling the data

--- juggling-async.js
+++ juggling-async.js
@@ -31,16 +31,42 @@ Promise.all([
 	handleDataCall(search[2]),
 ]).then((data) => {
 	data.forEach((block) => {
-    console.log(block + '\n');
+    console.log(block);
   })
 });
 
-// for(let i = 0; i < search.length; i++) {
-//   const promise = handleDataCall(search[i]).then(() => {
-//     console.log('done');
-//   })
-// }
+// ALTERNATE CODE TO DO THE SAME THING USING STREAM.PIPE()
 
-// handleDataCall(search[0]).then((res) => {
-// 	console.log(res);
-// });
+// 'use strict'
+//     const http = require('http')
+//     const bl = require('bl')
+//     const results = []
+//     let count = 0
+    
+//     function printResults () {
+//       for (let i = 0; i < 3; i++) {
+//         console.log(results[i])
+//       }
+//     }
+    
+//     function httpGet (index) {
+//       http.get(process.argv[2 + index], function (response) {
+//         response.pipe(bl(function (err, data) {
+//           if (err) {
+//             return console.error(err)
+//           }
+    
+//           results[index] = data.toString()
+//           count++
+    
+//           if (count === 3) {
+//             printResults()
+//           }
+//         }))
+//       })
+//     }
+    
+//     for (let i = 0; i < 3; i++) {
+//       httpGet(i)
+//     }
+
