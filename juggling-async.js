da80feff18cf41135fa4edd3e1104a11842dde11
7figureswagger <swistek.labs@gmail.com>
2020-07-02 18:28:05 -0700

    created function that returns a promise of data

--- juggling-async.js
+++ juggling-async.js
@@ -1,17 +1,37 @@
-const http = require('http');
+const http = require("http");
 
 const search = process.argv.slice(2);
-search.forEach((url) => {
-  console.log(url);
+console.log(search);
+
+function handleDataCall(url) {
+	let dataRecieved = "";
+
+	return new Promise((resolve, reject) => {
+		http
+			.get(url, (res) => {
+				res.setEncoding("utf8");
+
+				res.on("data", (piece) => {
+					dataRecieved += piece;
+				});
+
+				res.on("error", (error) => {
+					reject(error);
+				});
+				res.on("end", () => {
+					resolve(dataRecieved);
+				});
+			})
+			.on("error", console.error);
+	});
+}
+
+// for(let i = 0; i < search.length; i++) {
+//   const promise = handleDataCall(search[i]).then(() => {
+//     console.log('done');
+//   })
+// }
+
+handleDataCall(search[0]).then((res) => {
+	console.log(res);
 });
-
-http.get(process.argv.slice(2), (res) => {
-  
-
-  res.on('data', (chunk) => {
-    console.log(chunk)
-  })
-
-
-  res.end(console.log('end'))
-}).on('error', console.error)
