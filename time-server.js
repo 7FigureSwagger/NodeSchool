fee64d8207c589da47a8fed6dbd57783adc71e7b
7figureswagger <swistek.labs@gmail.com>
2020-07-02 23:10:11 -0700

    grabbing date for formatting

--- time-server.js
+++ time-server.js
@@ -1,9 +1,15 @@
 const net = require('net');
 const port = process.argv[2];
+
 const server = net.createServer(function(socket){
+
   console.log('server created');
+  let data = '';
+  let date = new Date();
+  data += date.getFullYear() + '-';
+  data += date.getMonth() + '-';
+  data += date.getDate();
+  console.log('data', data);
+  socket.end(data);
 });
 server.listen(port);
-
-
-console.log(process.argv[2]);
