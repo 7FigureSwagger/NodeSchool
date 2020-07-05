743d433c283f492ffcce9d39fd9f85c311259c8f
7figureswagger <swistek.labs@gmail.com>
2020-07-04 17:54:31 -0700

    create read stream for the file, pipe data to the response stream

--- http-file-server.js
+++ http-file-server.js
@@ -1,10 +1,18 @@
 const http = require('http');
-const input = process.argv.slice(2);
+const fs = require('fs');
 
-console.log(input);
+const input = process.argv.slice(2);
 
 const server = http.createServer((req, res) => {
   console.log('server opened');
+  const stream = fs.createReadStream(input[1])
+
+  setTimeout(() => {
+    stream.pipe(res);
+
+    stream.close();
+  }, 1000);
+
 })
 
 server.listen(input[0]);
