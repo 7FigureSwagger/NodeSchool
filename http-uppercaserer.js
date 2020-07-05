3b9626b72b2dd16e41f3ff32aaf03d199fc98ed7
7figureswagger <swistek.labs@gmail.com>
2020-07-05 00:31:56 -0700

    listening for data requests, converting to uppercase.

--- http-uppercaserer.js
+++ http-uppercaserer.js
@@ -1,10 +1,17 @@
-const http = require('http');
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+	console.log("server opened");
+	res.writeHead("200", { "content-type": "text/plain" });
 
-const server = http.createServer(() => {
-  console.log('server opened');
-
-  
+	setTimeout(() => {
+		let test = "";
+		req.on("data", (d) => {
+      console.log(d.toString().toUpperCase());
+      
+		});
+		// req.pipe(test);
+	}, 100);
 });
 
 server.listen(process.argv[2]);
